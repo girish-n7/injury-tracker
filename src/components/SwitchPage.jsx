355ba8131370f815d6e6469daca15895f1e312c8
Girish_N <ngirish1729@gmail.com>
2023-10-17 16:10:38 +0530

    View update

--- src/components/SwitchPage.jsx
+++ src/components/SwitchPage.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Report from "./Report";
 import View from "./View";
+import Edit from "./Edit";
 
 export default function SwitchPage() {
   return (
@@ -9,6 +10,7 @@ export default function SwitchPage() {
       <Route exact path="/" element={<Home />} />
       <Route exact path="/report" element={<Report />} />
       <Route exact path="/view/:id" element={<View />} />
+      <Route exact path="/edit/:id" element={<Edit />} />
     </Routes>
   );
 }
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,3 +1,71 @@
-export default function View() {
-  return <p>View</p>;
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getById } from "./API";
+import map from "../assets/body-map.png";
+
+export default function Report() {
+  let navigate = useNavigate();
+
+  //get id from route parameters
+  let { id } = useParams();
+
+  //manage state for injury
+  let [injury, setInjury] = useState();
+
+  //call backend api to get required injury using id
+  useEffect(() => {
+    getById(id)
+      .then((response) => response.json())
+      .then((result) => setInjury(result))
+      .catch((err) => console.error(err));
+  }, [id]);
+
+  //create circlesMap
+  let circlesMap = injury?.circles.map((item) => {
+    return (
+      <div
+        key={item.id}
+        className="circle"
+        style={{
+          position: "absolute",
+          top: `${item.y - 12.5}px`,
+          left: `${item.x - 12.5}px`,
+        }}
+      >
+        {item.id}
+      </div>
+    );
+  });
+
+  //   map the final injury details from injury object
+  let detailsMap = injury?.details.map((item) => {
+    return (
+      <div className="details--final" key={item.id}>
+        <p>Injury {item.id}</p>
+        <p>{item.details}</p>
+      </div>
+    );
+  });
+
+  return (
+    injury && (
+      <div className="report--container">
+        <div
+          className="report--map"
+          style={{ backgroundImage: `url(${map})`, position: "relative" }}
+        >
+          {circlesMap}
+        </div>
+        <div className="details--container">
+          <p>Name of the reporter: {injury.name}</p>
+          <p>Date of injury: {injury.injuryDate}</p>
+          <p>Time of injury: {injury.injuryTime}</p>
+          {detailsMap}
+          <button className="edit" onClick={() => navigate(`/edit/${id}`)}>
+            Edit
+          </button>
+        </div>
+      </div>
+    )
+  );
 }
